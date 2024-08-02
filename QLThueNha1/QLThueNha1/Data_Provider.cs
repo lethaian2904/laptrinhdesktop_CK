28830e725eac46c6dc76a6e83e9d7600261f1802
lethaian2904 <lethaian884@gmail.com>
2024-08-02 16:43:15 +0700

    thaian

---
+++ QLThueNha1/QLThueNha1/Data_Provider.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace QLThueNha1
+{
+    internal static class Data_Provider
+    {
+        private static SqlConnection cnn;
+        private static SqlDataAdapter da;
+        private static SqlCommand cmd;
+
+        public static void moKetNoi()
+        {
+            cnn = new SqlConnection();
+            cnn.ConnectionString= ConfigurationManager.ConnectionStrings["QLTN"].ConnectionString.ToString();
+            cnn.Open();
+        }
+
+        public static void dongKetNoi()
+        {
+            cnn.Close();
+        }
+
+        public static DataTable GetDataTable(string sql)
+        {
+            DataTable dt = new DataTable();
+            da = new SqlDataAdapter(sql, cnn);
+            da.Fill(dt);
+            return dt;
+        }
+
+        public static void updateData(string sql, object[] value =null, string[] name = null)
+        {
+            cmd = new SqlCommand(sql, cnn);
+            cmd.Parameters.Clear();
+            if (value != null)
+            {
+                for (int i = 0;i< value.Length; i++)
+                    cmd.Parameters.AddWithValue(name[i], value[i]);
+            }
+            cmd.ExecuteNonQuery();
+            cmd.Dispose();
+        }
+        public static int checkData(string sql)
+        {
+            int i = 0;
+            cmd = new SqlCommand(sql, cnn);
+            i = (int)cmd.ExecuteScalar();
+            cmd.Dispose();
+            return i;
+        }
+    }
+}
